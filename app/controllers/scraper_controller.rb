18b60878bd87ea97e7c8703bf09c53c482fbccec
Mark Reynoso <mreynoso@spu.edu>
2018-05-16 15:51:52 -0700

    looking for elements via scraping, not found

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -4,7 +4,13 @@ class ScraperController < ApplicationController
   def results
     response = HTTParty.get('http://www.subsplash.com/careers/')
     html_doc = Nokogiri::HTML(response)
-    li_tags = html_doc.css('li')
+    testing = html_doc.css("div.sqs-block-content")
+    output = []
+    testing.each do |one|
+      output.push(one.text)
+    end
+    @jobs = output
+    # li_tags = html_doc.css("li.BambooHR-ATS-Department-Header")
     binding.pry
     # @jobs = html_doc.css("BambooHR-ATS-Department-Header")
   end
